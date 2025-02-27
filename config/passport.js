a5a689716a9a8a271649940ac567af499d107284
oipi <90127483+oipithesecond@users.noreply.github.com>
2025-02-27 12:17:51 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'https://mess-plus-plus-backend.onrender.com/auth/google/callback',
+            callbackURL: 'https://messplusplusfe.vercel.app/auth/google/callback',
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
