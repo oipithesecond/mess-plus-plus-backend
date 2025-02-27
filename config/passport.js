00b97f9bf61f6af419227cf25bd2a2066ec2f9f6
oipi <tuberosumgames@gmail.com>
2025-02-27 22:31:23 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'http://localhost:2000/auth/google/callback',
+            callbackURL: 'https://mess-plus-plus-backend.onrender.com/auth/google/callback',
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
